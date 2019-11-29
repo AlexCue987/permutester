23111e40dd18101d6dec6e110ef3c57fde5ba9f1
Alexander.Kuznetsov <z002w5y@target.com>
2019-11-29 12:34:20 -0600

    ParameterizedTest-works

--- src/test/kotlin/unit/org/kollektions/permutester/ParameterizedExampleTest.kt
+++ src/test/kotlin/unit/org/kollektions/permutester/ParameterizedExampleTest.kt
@@ -1,11 +1,29 @@
 package org.kollektions.permutester
 
+import org.junit.jupiter.api.TestTemplate
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+import java.util.stream.Stream
+import kotlin.test.Test
+
 class ParameterizedExampleTest {
-//    @ParameterizedTest
-//    @MethodSource("validTokenProvider")
+
+    @ParameterizedTest
+    @MethodSource("validTokenProvider")
+//    @ValueSource(ints = [3, 0])
     fun `parse valid tokens`(data: Int) {
+        println(data)
     }
 
-    private fun validTokenProvider() = listOf(1, 2, 3)
+    @Test
+    fun runMe() {
+        println("runMe")
+    }
 
+    companion object {
+        @JvmStatic
+        private fun validTokenProvider() = Stream.of(1, 2, 3)
+    }
 }
+
