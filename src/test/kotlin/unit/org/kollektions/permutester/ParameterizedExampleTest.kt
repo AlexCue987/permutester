f55ec9c2543870abcf53011484f9ebb6e00abf6a
Alexander.Kuznetsov <z002w5y@target.com>
2019-11-29 11:12:29 -0600

    ParameterizedExampleTest.kt

---
+++ src/test/kotlin/unit/org/kollektions/permutester/ParameterizedExampleTest.kt
@@ -0,0 +1,11 @@
+package org.kollektions.permutester
+
+class ParameterizedExampleTest {
+//    @ParameterizedTest
+//    @MethodSource("validTokenProvider")
+    fun `parse valid tokens`(data: Int) {
+    }
+
+    private fun validTokenProvider() = listOf(1, 2, 3)
+
+}
